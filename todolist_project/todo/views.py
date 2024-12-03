4a33e37f32e9865776d015b17de3d51562ad1327
Janey9618 <163968692+Janey9618@users.noreply.github.com>
2024-12-03 04:26:18 -0500

    Add files via upload

---
+++ todolist_project/todo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.filter(completed=False)
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('task_list')
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
